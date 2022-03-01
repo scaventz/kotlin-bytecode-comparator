0bf2ef9b9c0e58c3e2a19d3dae61f0415bee18d9
Xin Wang <scaventz@outlook.com>
2022-03-02 00:05:36 +0800

    Replace SimpleDiffViewer with com.intellij.diff.DiffManager#createRequestPanel
    see https://intellij-support.jetbrains.com/hc/en-us/community/posts/4492346652946-How-to-update-request-of-SimpleDiffViewer-?page=1#community_comment_4495090900370

--- src/main/kotlin/com/scaventz/window/ComparatorForm.kt
+++ src/main/kotlin/com/scaventz/window/ComparatorForm.kt
@@ -2,25 +2,32 @@ package com.scaventz.window
 
 import com.intellij.diff.DiffContentFactory
 import com.intellij.diff.DiffContext
+import com.intellij.diff.DiffManager
 import com.intellij.diff.requests.ContentDiffRequest
 import com.intellij.diff.requests.SimpleDiffRequest
+import com.intellij.diff.tools.simple.SimpleDiffTool
 import com.intellij.diff.tools.simple.SimpleDiffViewer
 import com.intellij.diff.tools.util.base.HighlightPolicy
 import com.intellij.diff.tools.util.base.IgnorePolicy
 import com.intellij.diff.tools.util.base.TextDiffSettingsHolder
+import com.intellij.diff.util.DiffUserDataKeysEx
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.dsl.builder.*
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import com.intellij.ui.dsl.gridLayout.VerticalAlign
 import javax.swing.JPanel
 import javax.swing.event.DocumentEvent
 
 class ComparatorForm(val project: Project) {
     val panel: JPanel
 
-    private var differ = SimpleDiffViewer(Context, buildRequest("compiler 1", "compiler 1", "", "")).apply { init() }
+    private val diffPanel = DiffManager.getInstance().createRequestPanel(null, Disposer.newDisposable(), null).apply {
+        putContextHints(DiffUserDataKeysEx.FORCE_DIFF_TOOL, SimpleDiffTool.INSTANCE)
+    }
     private val log = Logger.getInstance(this::class.java)
 
     init {
@@ -33,7 +40,7 @@ class ComparatorForm(val project: Project) {
                 chooseBtn1.component.textField.document.addDocumentListener(
                     object : DocumentAdapter() {
                         override fun textChanged(e: DocumentEvent) {
-                            // how to update content of request of differ?
+                            diffPanel.setRequest(buildRequest("compiler1", "compiler2", "123", "124"))
                         }
                     }
                 )
@@ -61,8 +68,10 @@ class ComparatorForm(val project: Project) {
             }
 
             row {
-                cell(differ.component).horizontalAlign(HorizontalAlign.FILL)
-            }
+                cell(diffPanel.component)
+                    .horizontalAlign(HorizontalAlign.FILL)
+                    .verticalAlign(VerticalAlign.FILL)
+            }.resizableRow()
         }
     }
 
@@ -72,23 +81,3 @@ class ComparatorForm(val project: Project) {
         return SimpleDiffRequest("Window Title", content1, content2, title1, title2)
     }
 }
-
-object Context : DiffContext() {
-    init {
-        val settings = TextDiffSettingsHolder.TextDiffSettings()
-        settings.highlightPolicy = HighlightPolicy.BY_WORD
-        settings.ignorePolicy = IgnorePolicy.IGNORE_WHITESPACES
-        settings.contextRange = 5
-        settings.isExpandByDefault = true
-        settings.isEnableSyncScroll = true
-        putUserData(TextDiffSettingsHolder.TextDiffSettings.KEY, settings)
-    }
-
-    override fun isFocusedInWindow() = false
-    override fun requestFocusInWindow() {}
-    override fun getProject(): Project? = null
-    override fun isWindowFocused() = false
-}
-
-
-
