bb6e9bf7971216a13bde0b5ce165aac05af27ad5
Xin Wang <scaventz@outlook.com>
2022-03-17 16:45:30 +0800

    Experimental: Support process timed out (60s)
    Signed-off-by: Xin Wang <scaventz@outlook.com>

--- src/main/kotlin/com/scaventz/services/Kotlinc.kt
+++ src/main/kotlin/com/scaventz/services/Kotlinc.kt
@@ -8,6 +8,7 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
 
 class Kotlinc {
     private val log = Logger.getInstance(this::class.java)
@@ -53,6 +54,7 @@ class Kotlinc {
 }
 
 internal object Processes {
+    private val log = Logger.getInstance(this::class.java)
     private val NEWLINE = System.getProperty("line.separator")
 
     /**
@@ -67,6 +69,11 @@ internal object Processes {
             .redirectErrorStream(true)
         val process = pb.start()
         val result = StringBuilder(80)
+        if(!process.waitFor(60, TimeUnit.SECONDS)) {
+            process.destroy()
+        }
+        log.info("process isAlive: ${process.isAlive}")
+
         BufferedReader(InputStreamReader(process.inputStream)).use { `in` ->
             while (true) {
                 val line = `in`.readLine() ?: break
